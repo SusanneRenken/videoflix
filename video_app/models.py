d09ae4f7f6ca9588e780d074665f341aa3d073bf
SusanneRenken <renken.susanne@gmail.com>
2025-12-31 12:28:51 +0100

    M3: add video model

--- video_app/models.py
+++ video_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Video(models.Model):
+
+    TYPE_CHOICES = [
+        ('pending', 'Pending'),
+        ('processing', 'Processing'),
+        ('ready', 'Ready'),
+        ('error', 'Error'),
+    ]
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    original_file = models.FileField(upload_to="videos/originals/")
+    thumbnail = models.ImageField(
+        upload_to="videos/thumbnails/",
+        blank=True,
+        null=True
+    )
+    category = models.CharField(max_length=100)
+    status = models.CharField(max_length=15, choices=TYPE_CHOICES, default='pending')
+
+    def __str__(self):
+        return self.title
